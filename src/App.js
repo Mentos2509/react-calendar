0cd668f6f15b55d6c036abe6ed702348b562e3df
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-06-11 18:10:55 +0200

    Add main styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import './App.css';
 import Calender from './components/calender/Calender';
+
+
 
 function App() {
   return (
--- src/components/calender/Calender.js
+++ src/components/calender/Calender.js
@@ -3,6 +3,12 @@ import '../calender/calender.css'
 import SingleCell from '../single-cell/SingleCell'
 
 
+const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
+const renderDays = (daysOfWeek) => {
+  return daysOfWeek.map((day, index)=>{
+    return <SingleCell key={index}>{day}</SingleCell>
+})}
+
 function Calender() {
   return (
     <div className='main-calender-wrapper'>
@@ -12,13 +18,38 @@ function Calender() {
         <SingleCell special>Month 2023</SingleCell>
         <SingleCell>{">"}</SingleCell>
         <SingleCell>{">>"}</SingleCell>
-        <SingleCell>Mon</SingleCell>
-        <SingleCell>Tue</SingleCell>
-        <SingleCell>Wed</SingleCell>
-        <SingleCell>Thur</SingleCell>
-        <SingleCell>Fri</SingleCell>
-        <SingleCell>Sat</SingleCell>
-        <SingleCell>Sun</SingleCell>
+        {renderDays(daysOfWeek)}
+        <SingleCell>1</SingleCell>
+        <SingleCell>2</SingleCell>
+        <SingleCell>3</SingleCell>
+        <SingleCell>4</SingleCell>
+        <SingleCell>5</SingleCell>
+        <SingleCell>6</SingleCell>
+        <SingleCell>7</SingleCell>
+        <SingleCell>8</SingleCell>
+        <SingleCell>9</SingleCell>
+        <SingleCell>10</SingleCell>
+        <SingleCell>11</SingleCell>
+        <SingleCell>12</SingleCell>
+        <SingleCell>13</SingleCell>
+        <SingleCell>14</SingleCell>
+        <SingleCell>15</SingleCell>
+        <SingleCell>16</SingleCell>
+        <SingleCell>17</SingleCell>
+        <SingleCell>18</SingleCell>
+        <SingleCell>19</SingleCell>
+        <SingleCell>20</SingleCell>
+        <SingleCell>21</SingleCell>
+        <SingleCell>22</SingleCell>
+        <SingleCell>23</SingleCell>
+        <SingleCell>24</SingleCell>
+        <SingleCell>25</SingleCell>
+        <SingleCell>26</SingleCell>
+        <SingleCell>27</SingleCell>
+        <SingleCell>28</SingleCell>
+        <SingleCell>29</SingleCell>
+        <SingleCell>30</SingleCell>
+        <SingleCell>31</SingleCell>
       </div>
     </div>
   )
