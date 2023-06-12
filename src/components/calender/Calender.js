f5476a414b4b0b8b0a7756eee277bda163f3f827
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-06-12 16:30:57 +0200

    Add logic for days display + change months

--- src/components/calender/Calender.js
+++ src/components/calender/Calender.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../calender/calender.css'
 import SingleCell from '../single-cell/SingleCell'
-import {differenceInDays, endOfMonth, startOfMonth} from 'date-fns'
+import {differenceInDays, endOfMonth, startOfMonth, subMonths, addMonths, format} from 'date-fns'
 
 
 const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun']
@@ -17,6 +17,12 @@ function Calender({value,onChange}) {
   const endOfTheMonth = endOfMonth(value)
   const numberOfDaysInAMonth = differenceInDays(endOfTheMonth, beginningOfTheMonth ) + 1
   const arrayOfDaysInAMonth = Array.from({length :numberOfDaysInAMonth}, (_,i)=> i + 1)
+  const numberOfFirstDayInAMonth = (beginningOfTheMonth.getDay() + 6 ) % 7
+  const numberOfLastDayInAMonth = (endOfTheMonth.getDay() + 6) % 7
+  const arrayOfEmptySingleCell = Array.from({length :numberOfFirstDayInAMonth}, (_,i)=> i + 1)
+  const remainingDaysInCalender = 6 - numberOfLastDayInAMonth
+  const arrayRemainingDaysInCalender = Array.from({length :remainingDaysInCalender}, (_,i)=> i + 1)
+
 
 const renderDaysNumber = (days) => {
   return days.map((day,index)=>{
@@ -24,52 +30,30 @@ const renderDaysNumber = (days) => {
   })
 }
 
+const renderEmptySingleCell = (days) => {
+  return days.map((index)=>{
+    return <SingleCell key={index}></SingleCell>
+  })
+}
 
-  console.log(value)
-  console.log(beginningOfTheMonth)
-  console.log(endOfTheMonth)
-  console.log(Array.from({length :numberOfDaysInAMonth}, (_,i)=> i + 1))
+  const nextMonth = () => onChange(addMonths(value,1))
+  const nextYear = () => onChange(addMonths(value,12))
+  const previousMonth = () => onChange(subMonths(value,1))
+  const previousYear = () => onChange(subMonths(value,12))
+
+
   return (
     <div className='main-calender-wrapper'>
       <div className='grid-template'>
-        <SingleCell>{"<<"}</SingleCell>
-        <SingleCell>{"<"}</SingleCell>
-        <SingleCell special>Month 2023</SingleCell>
-        <SingleCell>{">"}</SingleCell>
-        <SingleCell>{">>"}</SingleCell>
+        <SingleCell onClick={previousYear}>{"<<"}</SingleCell>
+        <SingleCell onClick={previousMonth}>{"<"}</SingleCell>
+        <SingleCell special>{format(value, 'LLLL yyyy')}</SingleCell>
+        <SingleCell onClick={nextMonth}>{">"}</SingleCell>
+        <SingleCell onClick={nextYear}>{">>"}</SingleCell>
         {renderDaysNames(daysOfWeek)}
-        {/* <SingleCell>1</SingleCell>
-        <SingleCell>2</SingleCell>
-        <SingleCell>3</SingleCell>
-        <SingleCell>4</SingleCell>
-        <SingleCell>5</SingleCell>
-        <SingleCell>6</SingleCell>
-        <SingleCell>7</SingleCell>
-        <SingleCell>8</SingleCell>
-        <SingleCell>9</SingleCell>
-        <SingleCell>10</SingleCell>
-        <SingleCell>11</SingleCell>
-        <SingleCell>12</SingleCell>
-        <SingleCell>13</SingleCell>
-        <SingleCell>14</SingleCell>
-        <SingleCell>15</SingleCell>
-        <SingleCell>16</SingleCell>
-        <SingleCell>17</SingleCell>
-        <SingleCell>18</SingleCell>
-        <SingleCell>19</SingleCell>
-        <SingleCell>20</SingleCell>
-        <SingleCell>21</SingleCell>
-        <SingleCell>22</SingleCell>
-        <SingleCell>23</SingleCell>
-        <SingleCell>24</SingleCell>
-        <SingleCell>25</SingleCell>
-        <SingleCell>26</SingleCell>
-        <SingleCell>27</SingleCell>
-        <SingleCell>28</SingleCell>
-        <SingleCell>29</SingleCell>
-        <SingleCell>30</SingleCell>
-        <SingleCell>31</SingleCell> */}
+        {renderEmptySingleCell(arrayOfEmptySingleCell)}
         {renderDaysNumber(arrayOfDaysInAMonth)}
+        {renderEmptySingleCell(arrayRemainingDaysInCalender)}
       </div>
     </div>
   )
--- src/components/single-cell/SingleCell.js
+++ src/components/single-cell/SingleCell.js
@@ -1,10 +1,10 @@
 import './singleCell.css'
 
-function SingleCell({ special, children}) {
+function SingleCell({ special, children, onClick}) {
     const className = special? 'display-special' : 'display-regular';
 
   return (
-    <div className={className}>
+    <div className={className} onClick={onClick}>
       {children}
     </div>
   )
